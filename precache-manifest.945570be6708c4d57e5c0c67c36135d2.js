9dba3a395460c5d8d7b788307f5d7d603fccd003
Cesar <mail.mansilla@gmail.com>
2020-09-07 20:19:49 -0300

    Updates

---
+++ precache-manifest.945570be6708c4d57e5c0c67c36135d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34f67c9aa7ae7f73f01b7993d87ec0e2",
+    "url": "/rn-the-dino-repository/index.html"
+  },
+  {
+    "revision": "27be587f1c9fe24cb01a",
+    "url": "/rn-the-dino-repository/static/css/main.b4c224b8.chunk.css"
+  },
+  {
+    "revision": "a9d09e7b9b02bf370002",
+    "url": "/rn-the-dino-repository/static/js/2.bb8bd084.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rn-the-dino-repository/static/js/2.bb8bd084.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27be587f1c9fe24cb01a",
+    "url": "/rn-the-dino-repository/static/js/main.6b4d60ef.chunk.js"
+  },
+  {
+    "revision": "04a8d2b78a6b45404fb0",
+    "url": "/rn-the-dino-repository/static/js/runtime-main.eb39d49a.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/rn-the-dino-repository/precache-manifest.945570be6708c4d57e5c0c67c36135d2.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/rn-the-dino-repository/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
